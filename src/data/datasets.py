9a6ed1c030de3cf951adb519cd35f0357f97e837
Tobias Skovgaard Jepsen <tobiasskovgaardjepsen@gmail.com>
2018-10-17 09:47:04 +0200

    Further analysis on ZKC with DeepWalk

---
+++ src/data/datasets.py
@@ -0,0 +1,11 @@
+from networkx import read_edgelist
+
+def load_karate_club():
+    edgelist_path = ''
+    read_edgelist(edgelist_path, nodetype=int)
+    # Read attributes file
+    # Annotate network
+    # Split members in training and test set
+    # Include instructor and administrator in training set
+    # Split in training set that should 
+    pass
